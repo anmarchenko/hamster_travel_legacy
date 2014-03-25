8cf55f9ffdb4292a9a50d7463edb77c73827fc0e
altmer <igendou@gmail.com>
2014-03-25 09:35:24 +0400

    Модель Users #TRAVEL-6

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,9 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  # Travel fields
+  field :home_town
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
