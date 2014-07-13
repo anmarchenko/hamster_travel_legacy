81f4fe060e8b0b5a20cc097f0230a2c4414a2947
altmer <igendou@gmail.com>
2014-07-13 15:38:06 +0400

    TRAVEL-30: UsersController spec - describe edit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,7 @@
 describe UsersController do
+
+  let(:attrs) {FactoryGirl.build(:user).attributes}
+
   describe '#show' do
     context 'when user is logged in' do
       login_user
@@ -45,5 +48,64 @@ describe UsersController do
       end
     end
   end
+
+  describe '#edit' do
+    context 'when user is logged in' do
+      login_user
+
+      context 'and when there is user' do
+        it 'renders edit template' do
+          get 'edit', id: subject.current_user.id
+          expect(response).to render_template 'users/edit'
+        end
+      end
+
+      context 'and when user wants to edit someone other than himself' do
+        let(:new_user) {FactoryGirl.create(:user)}
+        it 'redirects to dashboard with error' do
+          get 'edit', id: new_user.id
+          expect(response).to redirect_to '/'
+          expect(flash[:error]).to eq I18n.t('errors.unathorized')
+        end
+      end
+
+      context 'and when there is no user' do
+        it 'heads 404' do
+          get 'edit', id: 'no user'
+          expect(response).to have_http_status 404
+        end
+      end
+    end
+
+    context 'when no logged user' do
+      let(:new_user) {FactoryGirl.create(:user)}
+      it 'redirects to sign in' do
+        get 'edit', id: new_user.id
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:update_attrs) {{ user: attrs.merge(home_town_text: 'new home town text')}}
+
+    context 'when user is logged in' do
+      login_user
+
+      context 'and when there is updated user' do
+      end
+
+      context 'and when there is no user' do
+        it 'heads 404' do
+          put 'update', update_attrs.merge(id: 'no_user_id')
+          expect(response).to have_http_status 404
+        end
+      end
+    end
+
+    context 'when no logged user' do
+    end
+  end
+
 
 end
