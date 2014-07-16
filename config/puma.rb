afbf98c3610c82463ce3c83039f8d3d6e01bc73a
altmer <igendou@gmail.com>
2014-07-16 21:29:37 +0400

    TRAVEL-93: capistrano

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 threads 1,1
 workers 2
 
-bind  "unix:///var/applications/travel_planner/shared/tmp/sockets/puma.sock"
-pidfile "/var/applications/travel_planner/current/tmp/pids/pid"
-state_path "/var/applications/travel_planner/current/tmp/pids/state"
+bind  "unix:///var/applications/travel_planner/shared/tmp/puma/puma.sock"
+pidfile "/var/applications/travel_planner/current/tmp/puma/pid"
+state_path "/var/applications/travel_planner/current/tmp/puma/state"
 
 activate_control_app
