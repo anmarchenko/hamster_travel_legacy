38aafed78e77c51d3cfdf6ab8681e84d0cb58bb5
altmer <igendou@gmail.com>
2015-04-04 12:38:23 +0200

    Make query counter remember queries

--- config/initializers/query_counter.rb
+++ config/initializers/query_counter.rb
@@ -4,13 +4,20 @@ module ActiveRecord
       0
     end
 
+    cattr_accessor :queries do
+      []
+    end
+
     IGNORED_SQL = [/^PRAGMA (?!(table_info))/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /^SHOW max_identifier_length/]
 
     def call(name, start, finish, message_id, values)
       # FIXME: this seems bad. we should probably have a better way to indicate
       # the query was cached
       unless 'CACHE' == values[:name]
-        self.class.query_count += 1 unless IGNORED_SQL.any? { |r| values[:sql] =~ r }
+        unless IGNORED_SQL.any? { |r| values[:sql] =~ r }
+          self.class.query_count += 1
+          self.class.queries << values[:sql]
+        end
       end
     end
   end
@@ -22,8 +29,9 @@ module ActiveRecord
   class Base
     def self.count_queries(&block)
       ActiveRecord::QueryCounter.query_count = 0
+      ActiveRecord::QueryCounter.queries = []
       yield
-      ActiveRecord::QueryCounter.query_count
+      [ActiveRecord::QueryCounter.query_count, ActiveRecord::QueryCounter.queries]
     end
   end
 end
