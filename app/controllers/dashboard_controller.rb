db1488517a56ab910f4ce677d6517b65364aef26
Andrey Marchenko <igendou@imac-altmer-2.local>
2015-06-19 20:12:13 +0200

    New dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
 
+  layout 'dashboard'
+
   def index
-    @trips = Travels::Trip.where(private: false).order(created_at: :desc).page(1).per(3)
   end
 
 end
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -8,23 +8,6 @@ describe DashboardController do
       get 'index'
       expect(response).to be_success
       expect(response).to render_template('dashboard/index')
-      trips = assigns(:trips).to_a
-      expect(trips.count).to eq 3
-      trips.each_index do |i|
-        expect(trips[i].private).to eq false
-        next if trips[i+1].blank?
-        expect(trips[i].created_at).to be >= (trips[i+1].created_at)
-      end
-    end
-  end
-
-  context 'when there are no trips' do
-    before {Travels::Trip.destroy_all}
-    it 'should get index and retrieve first 3 trips' do
-      get 'index'
-      expect(response).to be_success
-      expect(response).to render_template('dashboard/index')
-      expect(assigns(:trips).to_a.count).to eq 0
     end
   end
 
