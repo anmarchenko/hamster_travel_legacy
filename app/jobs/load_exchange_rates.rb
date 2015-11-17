f3ed982db98c2502902bf2c0653881ce5ca54a43
altmer <igendou@gmail.com>
2015-11-17 20:59:59 +0100

    Resque task for loading rates from ECB

--- app/jobs/load_exchange_rates.rb
+++ app/jobs/load_exchange_rates.rb
@@ -2,8 +2,11 @@ class LoadExchangeRates
   @queue = :load_exchange_rates
 
   def self.perform
-    @bank = Money.default_bank
-    @bank.save_rates(ApplicationController::CACHE_RATES)
+    current_rates = ExchangeRate.current
+    return if current_rates && current_rates.rates_date == Date.today
+    bank = Money.default_bank
+    eu_file = bank.save_rates_to_s
+    ExchangeRate.create(eu_file: eu_file, rates_date: Date.today)
   end
 
 end
