4418d6bf9eb8710fe3dbeeed0617b2b18e6bda68
altmer <igendou@gmail.com>
2015-12-18 21:40:53 +0100

    In development save photos to S3

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -8,7 +8,7 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  if Rails.env.development? || Rails.env.test?
+  if Rails.env.test?
     datastore :file,
       root_path: Rails.root.join('public/system/dragonfly', Rails.env),
       server_root: Rails.root.join('public')
