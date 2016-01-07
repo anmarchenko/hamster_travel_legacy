d08c05d84b0756e90b4b08d4ffc8d01a502fe34f
altmer <igendou@gmail.com>
2016-01-07 15:25:01 +0100

    For cities autocomplete show flags

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -6,11 +6,14 @@ module Api
       term = params[:term] || ''
       respond_with [] and return if term.blank? || term.length < 3
       # cache here by term
-      res = Rails.cache.fetch("cities_by_#{term}_#{I18n.locale}_2015_12_19_3", :expires_in => 1.year.to_i) do
+      res = Rails.cache.fetch("cities_by_#{term}_#{I18n.locale}_2016_01_07", :expires_in => 1.year.to_i) do
         query = Geo::City.find_by_term(term).page(1)
-        query.collect { |city| {name: city.translated_name(I18n.locale),
-                                text: city.translated_text(with_region: true, with_country: true, locale: I18n.locale),
-                                code: city.id} }
+        query.collect { |city| {
+            name: city.translated_name(I18n.locale),
+            text: city.translated_text(with_region: true, with_country: true, locale: I18n.locale),
+            code: city.id,
+            flag_image: ApplicationController.helpers.flag(city.country_code)
+        } }
       end
       respond_with res
     end
