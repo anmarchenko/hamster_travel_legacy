29c5f937f94823888e20902ddf5425f0f2dac3a5
altmer <igendou@gmail.com>
2016-05-26 18:17:39 +0200

    ah, fuck it

--- app/controllers/concerns/image_uploading.rb
+++ app/controllers/concerns/image_uploading.rb
@@ -20,26 +20,5 @@ module Concerns
       object.save
     end
 
-    VALID_IMAGE_SIGNATURES = [
-        "\x89PNG\r\n\x1A\n".force_encoding(Encoding::ASCII_8BIT), # PNG
-        "GIF87a".force_encoding(Encoding::ASCII_8BIT), # GIF87
-        "GIF89a".force_encoding(Encoding::ASCII_8BIT), # GIF89
-        "\xFF\xD8".force_encoding(Encoding::ASCII_8BIT) # JPEG
-    ].freeze
-
-    def file_is_image(temporary_file_path)
-      return false unless temporary_file_path
-
-      file_stream = File.new(temporary_file_path, 'r')
-      first_eight_bytes = file_stream.readpartial(8)
-      file_stream.close
-
-      VALID_IMAGE_SIGNATURES.each do |signature|
-        return true if first_eight_bytes.start_with?(signature)
-      end
-
-      false
-    end
-
   end
 end
