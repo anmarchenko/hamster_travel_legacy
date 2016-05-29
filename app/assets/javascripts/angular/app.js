a1d7a8cbade3f86643e940cc6b558fddd73c886d
altmer <igendou@gmail.com>
2016-05-29 14:30:33 +0200

    Disable debug info angularjs

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -2,4 +2,6 @@ angular.module('travel-services', []);
 
 angular.module('travel-components', ['ui.bootstrap', 'ui.sortable', 'ngCookies', 'ngAnimate', 'ngSanitize', 'ui.bootstrap-slider', 'sticky']);
 
-angular.module('travel', ['travel-services', 'travel-components']);
+angular.module('travel', ['travel-services', 'travel-components']).config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.debugInfoEnabled(false);
+}]);
