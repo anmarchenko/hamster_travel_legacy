5b2481b34bdf93d6bb185a930c19bf3174c46c80
altmer <igendou@gmail.com>
2016-07-09 16:07:07 +0200

    Delete cache when loading exchange rates

--- app/jobs/load_exchange_rates.rb
+++ app/jobs/load_exchange_rates.rb
@@ -8,6 +8,11 @@ class LoadExchangeRates
     eu_file = bank.save_rates_to_s
     bank.update_rates_from_s eu_file
     ExchangeRate.create(eu_file: eu_file, rates_date: bank.rates_updated_at)
+
+    # invalidate caches for all trips
+    Travels::Trip.find_each do |trip|
+      Calculators::Budget.new(trip).invalidate_cache!
+    end
   end
 
 end
