7e2d3706ab723a24a33acd0689ca56f2027861e1
altmer <igendou@gmail.com>
2016-09-05 21:16:46 +0200

    restrict trips search only to visible trips

--- app/services/finders/trips.rb
+++ app/services/finders/trips.rb
@@ -4,6 +4,7 @@ module Finders
 
     def search(term, user)
       query = Travels::Trip.all
+      query = restrict_query(query, user)
       query = query.where('name ILIKE ? OR countries_search_index ILIKE ?', "%#{term}%", "%#{term}%") if term.present?
       query
     end
@@ -13,14 +14,24 @@ module Finders
           order(status_code: :desc, start_date: :desc).includes(:author_user).page(page || 1)
     end
 
-    def for_user user, page
+    def for_user(user, page)
       user.trips.where.not(status_code: Travels::Trip::StatusCodes::DRAFT).order(start_date: :desc).
           includes(:author_user).page(page || 1)
     end
 
-    def drafts user, page
+    def drafts(user, page)
       user.trips.where(status_code: Travels::Trip::StatusCodes::DRAFT).
           order(dates_unknown: :asc, start_date: :desc, name: :asc).includes(:author_user).page(page || 1)
+    end
+
+    def restrict_query(query, user)
+      if user.present?
+        query.where.not(status_code: Travels::Trip::StatusCodes::DRAFT).where(private: false).or(
+          Travels::Trip.where(id: user.trip_ids)
+        )
+      else
+        query.where.not(status_code: Travels::Trip::StatusCodes::DRAFT).where(private: false)
+      end
     end
   end
 end
