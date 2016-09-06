3166a1517911ec0725eefe3f2916b6e188416e37
altmer <igendou@gmail.com>
2016-09-06 20:04:27 +0200

    still failing with imgCrop

--- app/assets/javascripts/travel/trip/trip-image.controller.js
+++ app/assets/javascripts/travel/trip/trip-image.controller.js
@@ -1,3 +1,28 @@
-angular.module('travel').controller('TripImageController', ['$scope', 'Upload', function ($scope, Upload) {
-    //
+angular.module('travel').controller('TripImageController', ['$scope', 'Upload', '$uibModal', function ($scope, Upload, $uibModal) {
+    $scope.upload = function ($files) {
+        if (!$files.length > 0) {
+            return;
+        }
+        const file = $files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = function (evt) {
+            $uibModal.open({
+                animation: true,
+                templateUrl: 'tripImageCropModal.html',
+                controller: ['$scope', '$uibModalInstance', 'image', function ($scope, $uibModalInstance, image) {
+                    $scope.image = image;
+                    $scope.close = function () {
+                        $uibModalInstance.close();
+                    }
+                }],
+                resolve: {
+                    image: function() {
+                        return evt.target.result;
+                    }
+                }
+            })
+        };
+    };
 }]);
