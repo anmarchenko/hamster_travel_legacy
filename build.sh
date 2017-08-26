58b67c9fff834231f25a430cdd13d6206f3b4aed
altmer <igendou@gmail.com>
2017-08-26 23:06:46 +0200

    minor build changes

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t "altmer/hamster-travel-web:latest" .
+docker build -t "altmer/hamster-travel-web:latest" -f Dockerfile.production .
 docker push altmer/hamster-travel-web
